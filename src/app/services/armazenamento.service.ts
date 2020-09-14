ae635f3fd1f55247687670e8daed33903f595043
Samueletec2020 <60710058+Samueletec2020@users.noreply.github.com>
2020-09-14 16:36:16 -0300

    ARRUMANDO ERRO DO ARMAZENTAMENTO OFFLINE

--- src/app/services/armazenamento.service.ts
+++ src/app/services/armazenamento.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -9,36 +8,38 @@ export class ArmazenamentoService {
 
   constructor(private storage: Storage) { }
 
-  public salvarDados(chave: string, dados: any){
-    if(chave.trim().length > 0 && dados){
-      return this.storage.set(chave, dados) 
-      .then(()=> {
-        console.log('Dados armazenados com sucesso !');
-        return true;
-      })
-      .catch(erro => {
-        console.log('Erro ao gravar os dados', erro);
-        return false;
-      });
-    }else{
+  public salvarDados(chave: string, dados: any) {
+
+    if (chave.trim().length > 0 && dados) {
+      return this.storage.set(chave, dados)
+        .then(() => {
+          console.log('Dados armazenados com sucesso!');
+          return true;
+        })
+        .catch(erro => {
+          console.log('Erro ao gravar os dados', erro);
+          return false;
+        });
+    } else {
       return false;
     }
 
-  }//fim do salvar dados
+  }
 
-  public pegarDados(chave: string){
-    if(chave.trim.length > 0 ){
+  public pegarDados(chave: string) {
+
+    if (chave.trim().length > 0) {
       return this.storage.get(chave)
-      .then(dadosArmazenados => {
-        return dadosArmazenados;
-      })
-      .catch(erro =>{
-        console.log('Erro ao buscar os dados', erro);
-        return null;
-      });
-
-    }else {
-      return null;
+        .then(dadosArmazenados => {
+          return dadosArmazenados;
+        })
+        .catch(erro => {
+          console.log('Erro ao buscar os dados', erro);
+          return null;
+        });
+    } else {
+      return false;
     }
+
   }
 }
