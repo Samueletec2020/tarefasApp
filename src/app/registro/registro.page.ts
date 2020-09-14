97f1604f88593ed723c7602fa3dc2a4b674987a8
Samueletec2020 <60710058+Samueletec2020@users.noreply.github.com>
2020-09-14 15:15:54 -0300

    armazenamento Offline

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
-import { noUndefined } from '@angular/compiler/src/util';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CpfValidator } from '../validators/cpf-validator';
 import { ComparacaoValidator } from '../validators/comparacao-validator';
+import { UsuariosService } from '../services/usuarios.service';
+import { AlertController } from '@ionic/angular';
+import { Usuario } from '../models/Usuario';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registro',
@@ -12,78 +14,100 @@ import { ComparacaoValidator } from '../validators/comparacao-validator';
 })
 export class RegistroPage implements OnInit {
 
-  public formRegistro: FormGroup;
-  public validacao={
+  public formRegister: FormGroup;
+
+  public mensagens_validacao = {
     nome: [
-      {tipo: 'required', mensagem: 'O campo nome é obrigatório!'},
-      { tipo: 'minLength', mensagem: 'O nome deve ter no mínimo 3 caracteres' }
+      { tipo: 'required', mensagem: 'O campo Nome é obrigatório!' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres!' }
     ],
     cpf: [
-      {tipo: 'required', mensagem: 'O campo cpf é obrigatório!'},
-      { tipo: 'maxLength', mensagem: 'O cpf deve ter no máximo 14 caracteres' },
-      { tipo: 'minLength', mensagem: 'O cpf deve ter no mínimo 11 caracteres' },
-      {tipo: 'invalido', mensagem: 'Cpf inválido!'}
+      { tipo: 'required', mensagem: 'O campo CPF é obrigatório!' },
+      { tipo: 'minlength', mensagem: 'O CPF deve ter pelo menos 11 caracteres!' },
+      { tipo: 'maxlength', mensagem: 'O CPF deve ter no máximo 14 caracteres!' },
+      { tipo: 'invalido', mensagem: 'CPF inválido' }
     ],
-    data: [
-      {tipo: 'required', mensagem: 'O campo data de nascimento é obrigatório!'}
+    dataNascimento: [
+      { tipo: 'required', mensagem: 'O campo Data de Nascimento é obrigatório!' }
     ],
     genero: [
-      {tipo: 'required', mensagem: 'O campo gênero é obrigatório!'}
+      { tipo: 'required', mensagem: 'Escolha um gênero!' }
     ],
     celular: [
-      { tipo: 'maxLength', mensagem: 'O campo celular tem no máximo 16 caracteres' }
+      { tipo: 'minlength', mensagem: 'O celular deve ter pelo menos 10 caracteres!' },
+      { tipo: 'maxlength', mensagem: 'O celular deve ter no máximo 16 caracteres!' }
     ],
     email: [
-      {tipo: 'required', mensagem: 'O campo email é obrigatório!'},
-      { tipo: 'email', mensagem: 'email inválido' }
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório!' },
+      { tipo: 'email', mensagem: 'E-mail inválido!' }
     ],
     senha: [
-      {tipo: 'required', mensagem: 'O campo senha é obrigatório' },
-      {tipo: 'minLength', mensagem: 'A senha deve ter no mínimo 6 caracteres' }
+      { tipo: 'required', mensagem: 'O campo senha é obrigatório!' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres!' }
     ],
-
-    confirmarsenha: [
-      {tipo: 'required', mensagem: 'O campo confirmar senha é obrigatório' },
-      {tipo: 'minLength', mensagem: 'A senha deve ter no mínimo 6 caracteres' },
-      {tipo: 'comparacao', mensagem: 'Deve ser igual a senha'}
-    ]
+    confirmar_senha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar a senha!' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres!' },
+      { tipo: 'comparacao', mensagem: 'Deve ser igual a Senha!' }
+    ],
   };
-    
 
-    
-    
-    
-    
-
-
-
-  constructor(private formBuilder: FormBuilder) { 
-    this.formRegistro = formBuilder.group({
+  constructor(
+    private formBuilder: FormBuilder,
+    private usuariosService: UsuariosService,
+    public alertController: AlertController,
+    public router: Router
+  ) {
+    this.formRegister = formBuilder.group({
       nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      cpf: ['', Validators.compose([Validators.required, 
-            Validators.maxLength(14),
-             Validators.minLength(11),
-            CpfValidator.cpfValido])], 
-      data: ['', Validators.compose([Validators.required])],
+      cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(14), CpfValidator.cpfValido])],
+      dataNascimento: ['', Validators.compose([Validators.required])],
       genero: ['', Validators.compose([Validators.required])],
-      celular: ['', Validators.compose([ Validators.maxLength(16)])],
-      email: ['', Validators.compose([Validators.required, Validators.email])], 
+      celular: ['', Validators.compose([Validators.minLength(10), Validators.maxLength(16)])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
       senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
-      confirmarsenha: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+      confirmar_senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
     }, {
-    validator: ComparacaoValidator('senha','confirmarsenha')
+      validator: ComparacaoValidator('senha', 'confirmar_senha')
     });
-   }
-
-  ngOnInit() {
   }
 
-  public cadastro() {
-    if(this.formRegistro.valid){
-    console.log('Formulário válido');
-    }else{
-      console.log('Formulário inválido');
+  async ngOnInit() {
+    await this.usuariosService.buscarTodos();
+    console.log(this.usuariosService.listaUsuarios);
+  }
+
+  public async salvarFormulario() {
+    if (this.formRegister.valid) {
+
+      const usuario = new Usuario();
+      usuario.nome = this.formRegister.value.nome;
+      usuario.cpf = this.formRegister.value.cpf;
+      usuario.dataNascimento = new Date(this.formRegister.value.dataNascimento);
+      usuario.genero = this.formRegister.value.genero;
+      usuario.celular = this.formRegister.value.celular;
+      usuario.email = this.formRegister.value.email;
+      usuario.senha = this.formRegister.value.senha;
+
+      if (await this.usuariosService.salvar(usuario)) {
+        this.exibirAlerta('SUCESSO!', 'Usuário salvo com sucesso!');
+        this.router.navigateByUrl('/login');
+      } else {
+        this.exibirAlerta('ERRO!', 'Erro ao salvar o usuário!');
+      }
+
+    } else {
+      this.exibirAlerta('ADVERTENCIA!', 'Formulário inválido<br/>Verifique os campos do seu formulário!');
     }
   }
 
+  async exibirAlerta(titulo: string, mensagem: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensagem,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
