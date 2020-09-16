b7c25d956baecc10873bec76ef5a15bd44ad6834
Samueletec2020 <60710058+Samueletec2020@users.noreply.github.com>
2020-09-16 18:09:41 -0300

    logout

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,8 +1,13 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 import {FormBuilder,FormGroup,Validators} from '@angular/forms';
 import{ Router} from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 import { HomePage } from '../home/home.page';
+import { UsuariosService } from '../services/usuarios.service';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -29,25 +34,78 @@ export class LoginPage implements OnInit {
 
 
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
-    this.formLogin=formBuilder.group({
+  constructor(private formBuilder: FormBuilder, 
+    private router: Router, 
+    private usuarioService: UsuariosService,
+    public toastController: ToastController,
+    public alertController: AlertController) {
+    
+      this.formLogin=formBuilder.group({
       email: ['',Validators.compose([Validators.required, Validators.email])],
       senha: ['',Validators.compose([Validators.required, Validators.minLength(6)])],
-      
+      manterLogado: [false]
 
-    })
+    });
 
    }
 
   ngOnInit() {
   }
-  public login(){
+ 
+  
+
+    
+  
+    async ionViewWillEnter() {
+      const usuarioLogado = await this.usuarioService.buscarUsuarioLogado();
+      if (usuarioLogado && usuarioLogado.manterLogado) {
+        this.router.navigateByUrl('/home');
+        this.presentToast();
+      
+      }else{
+        this.usuarioService.removerUsuarioLogado();
+      }
+
+      
+    }
+ 
+ 
+  public async login(){
     if(this.formLogin.valid){
-     console.log('formulário válido') 
-     this.router.navigateByUrl('/home');
+      
+      const usuario =await this.usuarioService.login(this.formLogin.value.email, this.formLogin.value.senha);
+     
+      if(usuario){
+       usuario.manterLogado= this.formLogin.value.manterLogado;
+       this.usuarioService.salvarUsuarioLogado(usuario);
+      this.router.navigateByUrl('/home');
+      this.presentToast();
+      
+     }else{
+       this.presentAlert('ADVERTÊNCIA','USUÁRIO OU SENHA INVÁLIDOS');
+     }
+
     }else{
-console.log('Formulário inválido')
+        this.presentAlert('ERRO','Formulário inválido');
     }
   }
+  
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Login efetuado com sucesso',
+      duration: 2000
+    });
+    toast.present();
+  }
+  async presentAlert(titulo: string, mensagem: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message:mensagem,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
 
 }
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -34,4 +34,32 @@ export class UsuariosService {
 
     return await this.armazenamentoService.salvarDados('usuarios', this.listaUsuarios);
   }
+  public async login(email:string, senha: string) {
+    let usuario: Usuario;
+    await this.buscarTodos();
+
+    const listaTemporaria = this.listaUsuarios.filter(usuarioArmazenado =>{
+      return (usuarioArmazenado.email==email && usuarioArmazenado.senha==senha);
+ 
+    }); //retorna um array
+
+    if(listaTemporaria.length > 0){
+      usuario = listaTemporaria.reduce(item => item);
+    }
+
+    return usuario;
+    
+  }
+  public salvarUsuarioLogado(usuario: Usuario){
+    delete usuario.senha;
+    this.armazenamentoService.salvarDados('usuarioLogado',usuario);
+  }
+public async buscarUsuarioLogado(){
+  return await this.armazenamentoService.pegarDados('usarioLogado');
+}
+
+public async removerUsuarioLogado(){
+  return await this.armazenamentoService.removerDados('usarioLogado');
+}
+
 }
